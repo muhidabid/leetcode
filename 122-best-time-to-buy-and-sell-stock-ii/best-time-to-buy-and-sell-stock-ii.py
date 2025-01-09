ea82cadf59eefb974b0dd4234c3c4ee27a750660
Muhid Abid <muhidabid.cs@gmail.com>
2025-01-09 16:52:37 +0100

    Time: 3 ms (44.49%) | Memory: 13.2 MB (61.35%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -7,10 +7,8 @@ class Solution(object):
         tmp_profit = 0
         i = 0
         while i < len(prices)-1:
-            if prices[i+1] - prices[i] < 0:
-                i += 1 
-                continue
-            tmp_profit += prices[i+1] - prices[i]
+            if prices[i+1] - prices[i] > 0:
+                tmp_profit += prices[i+1] - prices[i]
             i += 1
 
         return tmp_profit    
